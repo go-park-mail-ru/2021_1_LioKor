2e297d80f0107e7d7b0f11a05060b21e9cd72baf
KoroLion <korolion31@yandex.ru>
2021-03-09 16:59:59 +0300

    api: refactoring with go fmt

--- internal/pkg/user/validators/validators.go
+++ internal/pkg/user/validators/validators.go
@@ -1,18 +1,18 @@
 package validators
 
 import (
-    "strings"
 	"regexp"
+	"strings"
 )
 
 func ValidateUsername(username string) bool {
-	username = strings.ToLower(username);
+	username = strings.ToLower(username)
 
-	checker, _ := regexp.Compile("^[A-Za-z0-9_]{3,}");
+	checker, _ := regexp.Compile("^[A-Za-z0-9_]{3,}")
 	matchedLen := len(checker.FindString(username))
 
 	if matchedLen == 0 || matchedLen < len(username) {
-		return false;
+		return false
 	}
-	return !strings.Contains(username, "admin");
+	return !strings.Contains(username, "admin")
 }
