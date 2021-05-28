094230ff360f0a9697b253de2d039dda84109c42
KoroLion <korolion31@yandex.ru>
2021-05-28 22:00:56 +0300

    fix: user errors

--- internal/pkg/user/delivery/handler.go
+++ internal/pkg/user/delivery/handler.go
@@ -2,7 +2,6 @@ package delivery
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/labstack/echo/v4"
 	"liokor_mail/internal/pkg/common"
 	"liokor_mail/internal/pkg/user"
@@ -52,7 +51,7 @@ func (h *UserHandler) Auth(c echo.Context) error {
 
 	err := json.NewDecoder(c.Request().Body).Decode(&creds)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	err = h.UserUsecase.Login(creds)
@@ -66,7 +65,7 @@ func (h *UserHandler) Auth(c echo.Context) error {
 	}
 	err = h.setSessionCookie(&c, creds.Username)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, errors.New("не удалось установить печеньки"))
+		return echo.NewHTTPError(http.StatusInternalServerError, "не удалось установить печеньки")
 	}
 
 	return c.String(http.StatusOK, "ok")
@@ -84,7 +83,7 @@ func (h *UserHandler) Logout(c echo.Context) error {
 		case common.InvalidSessionError:
 			return echo.NewHTTPError(http.StatusUnauthorized)
 		default:
-			return echo.NewHTTPError(http.StatusInternalServerError, errors.New("что-то пошло не так"))
+			return echo.NewHTTPError(http.StatusInternalServerError, "что-то пошло не так")
 		}
 	}
 
@@ -109,9 +108,9 @@ func (h *UserHandler) ProfileByUsername(c echo.Context) error {
 	if err != nil {
 		switch err.(type) {
 		case common.InvalidUserError:
-			return echo.NewHTTPError(http.StatusNotFound, errors.New("нет такого пользователя"))
+			return echo.NewHTTPError(http.StatusNotFound, "нет такого пользователя")
 		default:
-			return echo.NewHTTPError(http.StatusInternalServerError, errors.New("что-то пошло не так"))
+			return echo.NewHTTPError(http.StatusInternalServerError, "что-то пошло не так")
 		}
 	}
 
@@ -125,26 +124,26 @@ func (h *UserHandler) SignUp(c echo.Context) error {
 
 	err := json.NewDecoder(c.Request().Body).Decode(&newUser)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	err = h.UserUsecase.SignUp(newUser)
 	if err != nil {
 		switch err.(type) {
 		case common.InvalidUserError:
-			return echo.NewHTTPError(http.StatusConflict, errors.New("такое имя уже занято"))
+			return echo.NewHTTPError(http.StatusConflict, "такое имя уже занято")
 		case user.InvalidUsernameError:
-			return echo.NewHTTPError(http.StatusBadRequest, errors.New("попробуйте другое имя"))
+			return echo.NewHTTPError(http.StatusBadRequest, "попробуйте другое имя")
 		case user.WeakPasswordError:
-			return echo.NewHTTPError(http.StatusBadRequest, errors.New("слишком слабый пароль"))
+			return echo.NewHTTPError(http.StatusBadRequest, "слишком слабый пароль")
 		default:
-			return echo.NewHTTPError(http.StatusBadRequest, errors.New("что-то пошло не так"))
+			return echo.NewHTTPError(http.StatusBadRequest, "что-то пошло не так")
 		}
 	}
 
 	err = h.setSessionCookie(&c, newUser.Username)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, errors.New("не удалось установить печеньки"))
+		return echo.NewHTTPError(http.StatusInternalServerError, "не удалось установить печеньки")
 	}
 
 	return c.String(http.StatusOK, "вы успешно зарегестрированы")
@@ -159,7 +158,7 @@ func (h *UserHandler) UpdateProfile(c echo.Context) error {
 
 	username := c.Param("username")
 	if username != sessionUser.Username {
-		return echo.NewHTTPError(http.StatusUnauthorized, errors.New("не лезь"))
+		return echo.NewHTTPError(http.StatusUnauthorized, "не лезь")
 	}
 
 	newData := user.User{}
@@ -168,7 +167,7 @@ func (h *UserHandler) UpdateProfile(c echo.Context) error {
 
 	err := json.NewDecoder(c.Request().Body).Decode(&newData)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	sessionUser, err = h.UserUsecase.UpdateUser(sessionUser.Username, newData)
@@ -193,7 +192,7 @@ func (h *UserHandler) UploadImage(c echo.Context) error {
 	defer c.Request().Body.Close()
 	err := json.NewDecoder(c.Request().Body).Decode(&uploadImage)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	imagePath, err := h.UserUsecase.UploadImage(sessionUser.Username, uploadImage.DataUrl)
@@ -228,7 +227,7 @@ func (h *UserHandler) UpdateAvatar(c echo.Context) error {
 
 	err := json.NewDecoder(c.Request().Body).Decode(&newAvatar)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	sessionUser, err = h.UserUsecase.UpdateAvatar(sessionUser.Username, newAvatar.AvatarUrl)
@@ -258,12 +257,12 @@ func (h *UserHandler) ChangePassword(c echo.Context) error {
 
 	err := json.NewDecoder(c.Request().Body).Decode(&changePassword)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("неверный формат json"))
+		return echo.NewHTTPError(http.StatusBadRequest, "неверный формат json")
 	}
 
 	err = h.UserUsecase.ChangePassword(sessionUser, changePassword)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("не получилось поменять пароль"))
+		return echo.NewHTTPError(http.StatusBadRequest, "не получилось поменять пароль")
 	}
 
 	return c.String(http.StatusOK, "")
